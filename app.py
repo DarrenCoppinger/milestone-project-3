e67b28a3b30340aee43d43c114c645caf3d7dea7
DarrenCoppinger <darrencoppinger@gmail.com>
2020-02-17 18:56:17 +0000

    connected to mongoDB and set up environmental variable for MONGO_URI

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'karaokean'
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def test():
-    return 'Test'
+@app.route('/index')
+def home():
+    return render_template("index.html", categories=mongo.db.categories.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
