ac7231d3f5109c96ee08e1461878e29478fc1ec5
DarrenCoppinger <darrencoppinger@gmail.com>
2020-02-20 17:00:36 +0000

    register and login pages connected to database#

--- app.py
+++ app.py
@@ -11,19 +11,55 @@ app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'karaokean'
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+app.config["secret_key"] = os.environ.get('SECRET_KEY')
 
 mongo = PyMongo(app)
 
-@app.route('/')
-@app.route('/index')
-def home():
-    return render_template("index.html", categories=mongo.db.categories.find())
 
-@app.route('/<password>')
-def 
+@app.route('/')
+def index():
+    message = session['username']
+    # if 'username' in session:
+    #     return 'You are logged in as ' + 
+
+    return render_template("index.html", message=message)
+
+@app.route('/loginpage')
+def loginpage():
+    return render_template("loginpage.html")
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    users = mongo.db.users
+    login_user = users.find_one({'name': request.form['username']})
+
+    if login_user:  
+        if check_password_hash(login_user['password'], request.form['password']):
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+
+        return 'Invalid username/password combination'
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'name': request.form['username']})
+
+        if existing_user is None:
+            hashed_value = generate_password_hash(request.form['password']) 
+            users.insert({'name' : request.form['username'], 'password' : hashed_value})
+            session['username'] = request.form['username']
+            return redirect(url_for('login'))
+    
+        return 'That Username already exists'
+
+    return render_template('register.html')
+
 
 if __name__ == '__main__':
-    app.secret_key = 'mysercet'
+    app.secret_key = 'mysecret'
     app.run(host=os.environ.get('IP'),
     port=int(os.environ.get('PORT')),
     debug=True)
