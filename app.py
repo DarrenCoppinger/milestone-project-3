3058f52fe9c245ea567c8c30cc310f38bb21f4ab
DarrenCoppinger <darrencoppinger@gmail.com>
2020-02-25 18:07:42 +0000

    Added catalogue.html page and populated page with videos from database

--- app.py
+++ app.py
@@ -20,9 +20,10 @@ mongo = PyMongo(app)
 def index():
     message = session['username']
     # if 'username' in session:
-    #     return 'You are logged in as ' + 
+    #     return 'You are logged in as ' +
 
     return render_template("index.html", message=message)
+
 
 @app.route('/loginpage')
 def loginpage():
@@ -34,12 +35,13 @@ def login():
     users = mongo.db.users
     login_user = users.find_one({'name': request.form['username']})
 
-    if login_user:  
+    if login_user:
         if check_password_hash(login_user['password'], request.form['password']):
             session['username'] = request.form['username']
             return redirect(url_for('index'))
 
         return 'Invalid username/password combination'
+
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
@@ -48,27 +50,54 @@ def register():
         existing_user = users.find_one({'name': request.form['username']})
 
         if existing_user is None:
-            hashed_value = generate_password_hash(request.form['password']) 
-            users.insert({'name' : request.form['username'], 'password' : hashed_value})
+            hashed_value = generate_password_hash(request.form['password'])
+            users.insert(
+                {'name': request.form['username'], 'password': hashed_value})
             session['username'] = request.form['username']
             return redirect(url_for('login'))
-    
+
         return 'That Username already exists'
 
     return render_template('register.html')
 
+
 @app.route('/addtrack')
-def addsong():
+def addtrack():
     return render_template('addtrack.html')
+
 
 @app.route('/insert_track', methods=['POST'])
 def insert_track():
     tracks = mongo.db.tracks
-    tracks.insert_one(request.form.to_dict())
-    return redirect(url_for('index'))
+    # tracks.insert_one(request.form.to_dict())
+    try:
+        tracks.insert_one(
+            {
+                'name': request.form.get('track_name'),
+                'artist': request.form.get('artist_name'),
+                'year': int(request.form.get('year')),
+                'genre': request.form.get('genre_name'),
+                'lyrics': request.form.get('youtube_link'),
+                'video': request.form.get('video_link'),
+                'likes': 1
+            }
+        )     
+        
+        return redirect(url_for('index'))
+
+    except pymongo.errors.DuplicateKeyError:
+        return redirect('addtrack')
+
+@app.route('/catalogue')
+def catalogue():
+    tracks = mongo.db.tracks.find()
+    all_tracks = mongo.db.tracks
+    tracks_total = all_tracks.count()
+    return render_template('catalogue.html', tracks=tracks, tracks_total=tracks_total)
+
 
 if __name__ == '__main__':
-    app.secret_key = 'mysecret'
+    app.secret_key = 'secret_key'
     app.run(host=os.environ.get('IP'),
-    port=int(os.environ.get('PORT')),
-    debug=True)
+            port=int(os.environ.get('PORT')),
+            debug=True)
---
+++ static/js/validation.js
@@ -0,0 +1,40 @@
+var form = document.getElementsByTagName('form')[0],
+    track_name = document.getElementById('track_name'),
+    video_link = document.getElementById('video_link');
+
+
+/* Below solution found here on Stackoverflow: https://stackoverflow.com/questions/28735459/how-to-validate-youtube-url-in-client-side-in-text-box */
+function validateYouTubeUrl() {
+    var url = $('#video_link').val();
+
+    if (url != undefined || url != '') {
+        var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=)([^#\&\?]*).*/;
+        var match = url.match(regExp);
+        if (match && match[2].length == 11) {
+            // Do anything for being valid
+            // if need to change the url to embed url then use below line            
+            //$('#videoObject').attr('src', 'https://www.youtube.com/embed/' + match[2] + '?autoplay=1&enablejsapi=1');
+            alert(match[2]);
+            return;
+        } else {
+            $("#video_link").removeClass("valid");
+            $("#video_link").addClass("invalid");
+            $("#video_link").addClass("active");
+            $("#video_link").prop("aria-invalid", "true");
+            event.preventDefault();
+        }
+    }
+}
+
+form.addEventListener("submit", function (event) {
+    if (!track_name.validity.valid) {
+        $("#track_name").removeClass("valid");
+        $("#track_name").addClass("invalid");
+        $("#track_name_label").addClass("active");
+        $("#track_name").prop("aria-invalid", "true");
+        event.preventDefault();
+    }
+    
+    validateYouTubeUrl();
+
+}, false);
