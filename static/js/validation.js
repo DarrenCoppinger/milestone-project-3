ba133f6a99fef84c4c6656937bc9a39a0c90a1ff
DarrenCoppinger <darrencoppinger@gmail.com>
2020-02-25 23:56:16 +0000

    Added buttons to each track entry presented on the catalogue.html page

--- static/js/validation.js
+++ static/js/validation.js
@@ -14,7 +14,7 @@ function validateYouTubeUrl() {
             // Do anything for being valid
             // if need to change the url to embed url then use below line            
             //$('#videoObject').attr('src', 'https://www.youtube.com/embed/' + match[2] + '?autoplay=1&enablejsapi=1');
-            alert(match[2]);
+            // alert(match[2]);
             return;
         } else {
             $("#video_link").removeClass("valid");
